b69cb5a8bf203d98dee4a4eadcdc936aef6793bd
kalmeshwar.bhavi <kalmeshwar.bhavi@rakuten.com>
2018-03-28 01:17:51 +0900

    project structure

---
+++ src/main/java/com/practice/MyExamples/MyExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.practice.MyExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExamplesApplication.class, args);
+	}
+}
